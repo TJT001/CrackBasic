3a686eb5a082711ddcd99a7622f994a15c65972e
j t <t1139696587@outlook.com>
2020-02-15 23:20:57 +0800

    画了一个类excel的表格

--- GDI03/GDI03.cpp
+++ GDI03/GDI03.cpp
@@ -18,17 +18,44 @@ IMPLEMENT_DYNCREATE(CMainWindow, CFrameWnd)
 
 BEGIN_MESSAGE_MAP(CMainWindow, CFrameWnd)
 
+	ON_WM_CREATE()
 	ON_WM_LBUTTONDOWN()
 	ON_WM_RBUTTONDOWN()
 	ON_WM_PAINT()
 
+	ON_WM_HSCROLL()
+	ON_WM_VSCROLL()
+
+	ON_WM_SIZE()
 END_MESSAGE_MAP()
 
 
 CMainWindow::CMainWindow()
+	:m_cellWidth(0),
+	 m_cellHeight(0),
+	 m_ribbonWidth(0),
+	 m_viewWidth(0),
+	 m_viewHeight(0)
 {
-	RECT rect = { 50, 50, 800, 700 };
-	Create(NULL, L"GDI基础03", WS_OVERLAPPEDWINDOW, rect);
+	/*RECT rect = { 50, 50, 800, 700 };
+	Create(NULL, L"GDI基础03", WS_OVERLAPPEDWINDOW, rect);*/
+	this->Create(NULL, L"Excel", WS_OVERLAPPEDWINDOW | WS_HSCROLL | WS_VSCROLL);
+}
+
+int CMainWindow::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if(CFrameWnd::OnCreate(lpCreateStruct) == -1)
+	return -1;
+
+	CClientDC dc(this);
+	m_cellWidth = dc.GetDeviceCaps(LOGPIXELSX);
+	m_cellHeight = dc.GetDeviceCaps(LOGPIXELSY);
+	m_ribbonWidth = m_cellWidth / 2;
+	m_viewWidth = (26 * m_cellWidth) + m_ribbonWidth;
+	m_viewHeight = m_cellHeight * 100;
+
+	m_hscrollPos = m_vscrollPos;
+	return 0;
 }
 
 void CMainWindow::OnLButtonDown(UINT nFlags, CPoint point)
@@ -61,7 +88,7 @@ void CMainWindow::OnPaint()
 	dc.SetTextColor(RGB(192, 190, 192));
 	dc.DrawText("MyFont", &rect, DT_VCENTER | DT_CENTER | DT_SINGLELINE);
 */
-	CPaintDC dc(this);
+	/*CPaintDC dc(this);
 	CPen pen;
 	CFont font;
 
@@ -106,7 +133,227 @@ void CMainWindow::OnPaint()
 	dc.Draw3dRect(&rect, RGB(255, 255, 255), RGB(128, 126, 128));
 	dc.SetBkMode(TRANSPARENT);
 	dc.DrawText("Caption", &rect, DT_VCENTER | DT_CENTER | DT_SINGLELINE);
+	*/
+
+	CPaintDC dc(this);
+	dc.SetWindowOrg(m_hscrollPos, m_vscrollPos);
+	CPen pen(PS_SOLID, 0, RGB(192, 192, 192));
+	CPen *pOldpen = dc.SelectObject(&pen);
+
+	for (int i = 0; i < 99; i++){
+
+		int y = (i * m_cellHeight) + m_cellHeight;
+		dc.MoveTo(0, y);
+		dc.LineTo(m_viewWidth, y);
+	}
+
+	for (int i = 0; i < 26; i++){
+		int x = (i * m_cellWidth) + m_ribbonWidth;
+		dc.MoveTo(x, 0);
+		dc.LineTo(x, m_viewHeight);
+	}
+
+	dc.SelectObject(pOldpen);
+
+	CBrush brush;
+	brush.CreateStockObject(LTGRAY_BRUSH);
+
+
+	CRect rcTop(0, 0, m_viewWidth, m_cellHeight);
+	dc.FillRect(rcTop, &brush);
+	CRect rcLeft(0, 0, m_ribbonWidth, m_viewHeight);
+
+	dc.FillRect(rcLeft, &brush);
+
+	dc.MoveTo(0, m_cellHeight);
+	dc.LineTo(m_viewWidth, m_cellHeight);
+	dc.MoveTo(m_ribbonWidth, 0);
+	dc.LineTo(m_ribbonWidth, m_viewHeight);
+
+	dc.SetBkMode(TRANSPARENT);
+
+
+
+	for (int i = 0; i < 99; i++){
+
+		int y = (i * m_cellHeight) + m_cellHeight;
+		dc.MoveTo(0, y);
+		dc.LineTo(m_ribbonWidth, y);
+
+		CString string;
+		string.Format(_T("%d"), i + 1);
+
+		CRect rect(0, y, m_ribbonWidth, y + m_cellHeight);
+
+		dc.DrawText(string, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+
+		rect.top++;
+		dc.Draw3dRect(rect, RGB(255, 255, 255), RGB(128, 128, 128));//3D效果
+	}
+
+
+
+	for (int j = 0; j < 26; j++)
+
+	{
+
+		int x = (j * m_cellWidth) + m_ribbonWidth;
+		dc.MoveTo(x, 0);
+		dc.LineTo(x, m_cellHeight);
+
+
+		CString string;
+		string.Format(_T("%c"), j + 'A');
+
+
+		CRect rect(x, 0, x + m_cellWidth, m_cellHeight);
+		dc.DrawText(string, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+
+
+
+		rect.left++;
+		dc.Draw3dRect(rect, RGB(255, 255, 255), RGB(128, 128, 128));
+	}
 
 }
 
 // 作业： 绘制一个excel表格
+
+void CMainWindow::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	int nDelta = 0;
+
+	switch (nSBCode)
+	{
+	case SB_LINELEFT:
+
+		nDelta = -LINESIZE;
+		break;
+
+	case SB_LINERIGHT:
+
+		nDelta = LINESIZE;
+		break;
+
+	case SB_PAGELEFT:
+
+		nDelta = -m_hpageSize;
+		break;
+	case SB_PAGERIGHT:
+
+		nDelta = m_hpageSize;
+		break;
+	case SB_THUMBTRACK:
+
+		nDelta = (int)nPos - m_hscrollPos;
+		break;
+	default:
+		break;
+	}
+
+	if (nDelta != 0)
+	{
+		m_hscrollPos += nDelta;
+		SetScrollPos(SB_HORZ, m_hscrollPos, TRUE);
+		ScrollWindow(-nDelta, 0);
+
+	}
+	CFrameWnd::OnHScroll(nSBCode, nPos, pScrollBar);
+}
+
+
+void CMainWindow::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	int nDelta = 0;
+
+	switch (nSBCode)
+	{
+	case SB_LINEDOWN:
+
+		nDelta = LINESIZE;
+		break;
+
+	case SB_LINEUP:
+
+		nDelta = -LINESIZE;
+		break;
+
+	case SB_THUMBTRACK:
+
+		nDelta = (int)nPos - m_hscrollPos;
+		break;
+
+	case SB_PAGEUP:
+		nDelta = -m_vpageSize;
+		break;
+
+	case SB_PAGEDOWN:
+
+		nDelta = m_vpageSize;
+		break;
+	default:
+		break;
+
+	}
+
+	if (nDelta != 0)
+	{
+
+		m_vscrollPos += nDelta;
+		SetScrollPos(SB_VERT, m_vscrollPos, TRUE);
+		ScrollWindow(0, -nDelta);
+	}
+
+	CFrameWnd::OnVScroll(nSBCode, nPos, pScrollBar);
+}
+
+
+void CMainWindow::OnSize(UINT nType, int cx, int cy)
+{
+	CFrameWnd::OnSize(nType, cx, cy);
+
+	int nHScrollMax = 0;
+	m_hpageSize = 0;
+
+
+	if (cx < m_viewWidth){
+
+		m_hpageSize = cx;
+		nHScrollMax = m_viewWidth - 1;
+	}
+
+
+
+	SCROLLINFO si;
+
+	si.fMask = SIF_PAGE | SIF_RANGE | SIF_POS;
+	si.nMin = 0;
+	si.nMax = nHScrollMax - 1;
+	si.nPos = m_hscrollPos;
+	si.nPage = m_hpageSize;
+
+
+	SetScrollInfo(SB_HORZ, &si, TRUE);
+
+	int nVScrollMax = 0;
+	m_vpageSize = 0;
+
+
+	if (cy < m_viewHeight){
+
+		nVScrollMax = m_viewHeight - 1;
+		m_vpageSize = cy;
+	}
+
+
+	si.fMask = SIF_PAGE | SIF_RANGE | SIF_POS;
+	si.nMin = 0;
+	si.nMax = nVScrollMax;
+	si.nPos = m_vscrollPos;
+	si.nPage = m_vpageSize;
+
+
+	SetScrollInfo(SB_VERT, &si, TRUE);
+}
--- GDI03/GDI03.h
+++ GDI03/GDI03.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <afxwin.h>
 
+#define LINESIZE 8
 
 class CMyApp :
 	public CWinApp
@@ -20,8 +21,25 @@ public:
 	CMainWindow();
 
 	DECLARE_MESSAGE_MAP()
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
 	afx_msg void OnPaint();
 
+	// 定义变量
+protected:
+	int m_cellWidth;   // 格子宽度
+	int m_cellHeight;  // 格子高度
+	int m_ribbonWidth;
+	int m_viewWidth;
+	int m_viewHeight;
+	int m_hscrollPos;
+	int m_vscrollPos;
+	int m_hpageSize;
+	int m_vpageSize;
+	
+public:
+	afx_msg void OnSize(UINT nType, int cx, int cy);
 };
